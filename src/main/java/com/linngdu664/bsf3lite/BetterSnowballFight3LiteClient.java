101b989ee6ae2b9836a8ab444b1b8fffec14cf53
zx1316 <1315862279@qq.com>
2026-05-11 21:48:33 +0800

    Initial commit

---
+++ src/main/java/com/linngdu664/bsf3lite/BetterSnowballFight3LiteClient.java
@@ -0,0 +1,31 @@
+package com.linngdu664.bsf3lite;
+
+import net.minecraft.client.Minecraft;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
+import net.neoforged.neoforge.client.gui.ConfigurationScreen;
+import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
+
+// This class will not load on dedicated servers. Accessing client side code from here is safe.
+@Mod(value = BetterSnowballFight3Lite.MODID, dist = Dist.CLIENT)
+// You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
+@EventBusSubscriber(modid = BetterSnowballFight3Lite.MODID, value = Dist.CLIENT)
+public class BetterSnowballFight3LiteClient {
+    public BetterSnowballFight3LiteClient(ModContainer container) {
+        // Allows NeoForge to create a config screen for this mod's configs.
+        // The config screen is accessed by going to the Mods screen > clicking on your mod > clicking on config.
+        // Do not forget to add translations for your config options to the en_us.json file.
+        container.registerExtensionPoint(IConfigScreenFactory.class, ConfigurationScreen::new);
+    }
+
+    @SubscribeEvent
+    static void onClientSetup(FMLClientSetupEvent event) {
+        // Some client setup code
+        BetterSnowballFight3Lite.LOGGER.info("HELLO FROM CLIENT SETUP");
+        BetterSnowballFight3Lite.LOGGER.info("MINECRAFT NAME >> {}", Minecraft.getInstance().getUser().getName());
+    }
+}
